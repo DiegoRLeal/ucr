1e60a87d4a127d7e8984a3e718c494bc2caa00d7
Diego Leal <leal.goedi@gmail.com>
2024-09-16 15:40:15 -0300

    Image

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create(full_name: 'Diego Leal', email: 'leal.goedi@gmail.com', password: '123456')
