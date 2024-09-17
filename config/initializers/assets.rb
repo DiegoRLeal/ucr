703687538c7de126464360d4bf660a602a48ea63
Diego Leal <leal.goedi@gmail.com>
2024-09-17 17:09:36 -0300

    PWA

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,10 @@ Rails.application.config.assets.version = "1.0"
 #
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
 
+Rails.application.config.public_file_server.headers = {
+  'Cache-Control' => 'no-store'
+}
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
