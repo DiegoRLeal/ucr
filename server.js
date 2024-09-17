caeb75e0bfb0eb430f36c544220a695f525441b4
Diego Leal <leal.goedi@gmail.com>
2024-09-17 18:16:57 -0300

    Adicionado módulo express

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('public'));
-
-app.listen(process.env.PORT, () => {
-  console.log('Server started on port ' + process.env.PORT);
-});
