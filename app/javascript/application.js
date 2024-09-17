acdd859c5277caa36e0cdad35e834faa84d3c934
Diego Leal <leal.goedi@gmail.com>
2024-09-17 17:37:27 -0300

    Pré-compilação de assets

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,2 @@
 import "@hotwired/turbo-rails"
 import "controllers"
-
-
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
-
-const application = Application.start()
-const context = require.context("controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
