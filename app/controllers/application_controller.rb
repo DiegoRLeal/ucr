5ec787d227d5872e590ac5d0a117c49427084661
Diego Leal <leal.goedi@gmail.com>
2024-09-18 14:52:00 -0300

    Sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource)
+    sidebar_path # Substitua por seu caminho de rota para sidebar.html.erb
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:full_name])
