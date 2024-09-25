67e9557f9c392c494b106c55ef3cb924928885b4
Diego Leal <leal.goedi@gmail.com>
2024-09-25 15:22:38 -0300

    BD

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,29 +26,4 @@ module ApplicationHelper
   def format_track_name(track_name)
     track_name.split('_').map(&:capitalize).join(' ')
   end
-
-  def points(position, penalty_value = 0)
-    base_points = case position
-    when 1 then 40
-    when 2 then 37
-    when 3 then 34
-    when 4 then 31
-    when 5 then 28
-    when 6 then 26
-    when 7 then 24
-    when 8 then 22
-    when 9 then 20
-    when 10 then 18
-    when 11 then 17
-    when 12 then 16
-    when 13 then 15
-    when 14 then 14
-    when 15 then 13
-    else 0
-    end
-
-    penalty_deduction = penalty_value * 10
-    total_points = base_points - penalty_deduction
-    total_points > 0 ? total_points : 0
-  end
 end
